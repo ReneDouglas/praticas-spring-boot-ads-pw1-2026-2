9800bfef883eaee8d6459bd025a7c38898dddd74
ReneDouglas <rene.douglas.bsi@gmail.com>
2026-09-11 21:07:55 -0300

    Aprendendo sobre controllers

---
+++ demo/src/main/java/com/example/demo/AlunosController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunosController {
+
+    @GetMapping(path = "/{matricula}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public String listar(
+            @PathVariable String matricula,
+            @RequestParam(required = false) String curso
+    ) {
+        if(matricula.equals("123") &&
+                (curso == null || curso.equals("ADS"))){
+            return "Aluno listado";
+        }
+        return "Aluno não encontrado";
+    }
+
+    @PostMapping
+    public String cadastrar(){
+        return "Cadastrar Aluno";
+    }
+}
